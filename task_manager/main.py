05a477ab3a7e08c9e1e1deb78d691c09c7eba6ff
Rudis Rozitis <rudisrozitis@RUDIS-PC.localdomain>
2025-11-14 15:08:41 +0200

    Added HTML and CSS pages

--- task_manager/main.py
+++ task_manager/main.py
@@ -4,13 +4,67 @@ from fastapi import FastAPI, HTTPException
 from models.task import Task
 import db
 from fastapi.encoders import jsonable_encoder
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, HTMLResponse
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return HTMLResponse("""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="/style.css">
+  <title>Task Manager</title>
+</head>
+<body>
+  <h1>TASK MANAGER</h1>
+
+    <form class="input-area">
+      <input type="text" placeholder="Input a task.." />
+      <button type="submit">ADD</button>
+    </form>
+
+    <div class="task-list">
+
+      <div class="task">
+        <div class="task-info">
+          <span class="date">Created: MM-DD-YYYY</span>
+          <span>Task1</span>
+        </div>
+        <button>Done</button>
+      </div>
+
+      <div class="task">
+        <div class="task-info">
+          <span class="date">Created: MM-DD-YYYY</span>
+          <span>Task2</span>
+        </div>
+        <button>Done</button>
+      </div>
+
+      <div class="task">
+        <div class="task-info">
+          <span class="date">Created: MM-DD-YYYY</span>
+          <span>Task3</span>
+        </div>
+        <button>Done</button>
+      </div>
+
+      <div class="task">
+        <div class="task-info">
+          <span class="date">Created: MM-DD-YYYY</span>
+          <span>Task4</span>
+        </div>
+        <button>Done</button>
+      </div>
+
+    </div>
+</body>
+</html>
+""")
 
 
 @app.post("/task")
